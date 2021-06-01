224ed942ee1251b87d71ef99372216d4dcc93b13
Chris Miraflor <chris.j.miraflor@gmail.com>
2021-05-31 18:38:30 -0700

    Wrote second test for if the leap year program can recognize that years divisible by 100 are not leap years.

--- test_leap_year.py
+++ test_leap_year.py
@@ -5,6 +5,10 @@ class Tester(unittest.TestCase):
   # Test that 2004 is recognized as a leap year, but 2003 is not.
   def test_div_by_4(self):
     self.assertTrue(leap_year.calc(2004) == True and leap_year.calc(2003) == False)
+  
+  # Test that 1900 is not recognzied as a leap year.
+  def test_div_by_100(self):
+    self.assertFalse(leap_year.calc(1900))
 
 if __name__ == '__main__':
   unittest.main()
