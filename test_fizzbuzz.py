279fe98bbc55d503abb2393415203a69db20c3d8
Chris Miraflor <chris.j.miraflor@gmail.com>
2021-05-31 18:14:55 -0700

    Wrote second test for if the fizzbuzz function returns 'Fizz' if the input is a multiple of 3.

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,6 +5,9 @@ class Tester(unittest.TestCase):
   # Test that FizzBuzz can return a number that is not a multiple of 3 or 5.
   def test_normal(self):
     self.assertEqual(fizzbuzz.calc(2), "2")
+  # Test if Fizz is returned for a number that is a multiple of 3.
+  def test_fizz(self):
+    self.assertEqual(fizzbuzz.calc(15), "Fizz")
 
 if __name__ == '__main__':
   unittest.main()
